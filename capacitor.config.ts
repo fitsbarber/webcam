c8b9dc734128222d79dd4fad352e6c07576bc0d9
Evan Oktavianus <evanoktavianus@gmail.com>
2022-06-12 21:45:30 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'barberfits-technology',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
